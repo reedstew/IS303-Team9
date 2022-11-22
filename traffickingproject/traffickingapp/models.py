823f1ce790139c87ce1474d2a55b27693506986f
ec134866 <evan@thecarters.com>
2022-11-22 16:25:12 -0700

    models & postgres

--- traffickingproject/traffickingapp/models.py
+++ traffickingproject/traffickingapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    date_missing = models.CharField(max_length=45)
+    age_at_missing = models.IntegerField()
+    city = models.CharField(max_length=45)
+    state = models.CharField(max_length=2)
+    gender = models.CharField(max_length=1)
+    race = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "people"
+
