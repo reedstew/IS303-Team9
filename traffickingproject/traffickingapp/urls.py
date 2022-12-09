04603106f359cf7d122841f555494bb972908825
reedstew <96210702+reedstew@users.noreply.github.com>
2022-12-08 23:51:53 -0700

    yup

--- traffickingproject/traffickingapp/urls.py
+++ traffickingproject/traffickingapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import indexPageView, victimsPageView, helpPageView
+from .views import searchPageView, addPageView
 
 urlpatterns = [
     path("", indexPageView, name="index"),
     path("victims/", victimsPageView, name="victims"),
+    path('search/', searchPageView, name="search"),
+    path('add/', addPageView, name="add"),
     path("help", helpPageView, name="help")
 ]
--- traffickingproject/traffickingapp/views.py
+++ traffickingproject/traffickingapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Person
 
 # Create your views here.
@@ -9,23 +9,71 @@ def indexPageView(request):
 
 
 def victimsPageView(request):
-    try: 
+    try:
         fName = request.GET['first_name']
-        
-        people = Person.objects.filter(first_name=fName) 
 
-    except: 
+        people = Person.objects.filter(first_name=fName)
+
+    except:
         people = Person.objects.all()
-    
+
     context = {
         "people": people
     }
     return render(request, 'traffickingapp/victims.html', context)
 
-# def searchPageView(Request): 
-#     fName = request.GET['first_name'] 
 
-#     return render(request)
+def searchPageView(request):
+    try:
+        fName = request.GET['first_name']
+
+        people = Person.objects.filter(first_name=fName)
+
+    except:
+        people = Person.objects.all()
+
+    context = {
+        "people": people
+    }
+    return render(request, 'traffickingapp/victims.html', context)
+
+
+def addPageView(request):
+    if request.method == 'POST':
+        # Add in a new person
+        # Get first_name, last_name, age_at_missing, city, state, gender, race
+        date_at_missing = request.POST['date_at_missing']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        age_at_missing = request.POST['age_at_missing']
+        city = request.POST['city']
+        state = request.POST['state']
+        gender = request.POST['gender']
+        race = request.POST['race']
+        # Create a book
+
+        new_person = Person()
+
+        new_person.date_at_missing = date_at_missing
+        new_person.first_name = first_name
+        new_person.last_name = last_name
+        new_person.age_at_missing = age_at_missing
+        new_person.city = city
+        new_person.state = state
+        new_person.gender = gender
+        new_person.race = race
+
+        new_person.save()
+
+        return redirect('index')
+    else:
+        people = Person.objects.all()
+
+        context = {
+            "people": people
+        }
+
+        return render(request, "traffickingapp/add.html", context)
 
 
 def helpPageView(request):
